e0163285e7f39dfd1a81ccfed0525550e5204b79
mary <151822188+marialuyb@users.noreply.github.com>
2023-12-08 06:25:05 +0000

    Update 4 files

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
-  <HeaderBlock/>
-  <MainBlock/>
+  <HeaderBlock />
+  <MainBlock />
+  <FooterBlock />
 </template>
 
 <script>
-import HeaderBlock from './components/HeaderBlock.vue' 
-import MainBlock from './components/MainBlock.vue' 
+import HeaderBlock from "./components/HeaderBlock.vue";
+import MainBlock from "./components/MainBlock.vue";
+import FooterBlock from "./components/FooterBlock.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
     HeaderBlock,
-    MainBlock
-  }
-}
+    MainBlock,
+    FooterBlock,
+  },
+};
 </script>
 
 <style>
@@ -21,41 +24,43 @@ export default {
   color: rgb(255, 255, 255);
   background: linear-gradient(90deg, #0e031d 0%, #2c146b 100%);
 }
-@font-face{
-    font-family: Open sans-serif;
-    src: url("./assets/OpenSans-VariableFont_wdth,wght.ttf")
+@font-face {
+  font-family: Open sans-serif;
+  src: url("./assets/OpenSans-VariableFont_wdth,wght.ttf");
 }
-html{
-    background: linear-gradient(90deg, #0e031d 0%, #2c146b 100%);
-    color: rgb(255, 255, 255);
-    font-family: Open sans-serif;
-    font-weight: 400;
-    width: 100%; 
+html {
+  background: linear-gradient(90deg, #0e031d 0%, #2c146b 100%);
+  color: rgb(255, 255, 255);
+  font-family: Open sans-serif;
+  font-weight: 400;
+  width: 100%;
 }
-*, *::before, *::after{
-    box-sizing: inherit;
+*,
+*::before,
+*::after {
+  box-sizing: inherit;
 }
-body{
-    width: 100%;
-    margin: 0 auto;
+body {
+  width: 100%;
+  margin: 0 auto;
 }
-a{
-    text-decoration: none;
-    color: white;
-    font-size: 12px;
+a {
+  text-decoration: none;
+  color: white;
+  font-size: 12px;
 }
-h1{
-    font-size: 18px;
+h1 {
+  font-size: 18px;
 }
-h2{
-    font-size: 16px;
+h2 {
+  font-size: 16px;
 }
-h3{
-    font-size: 8px;
+h3 {
+  font-size: 8px;
 }
-.wrapper{
-    width: 1200px;
-    height: auto;
-    margin: 0 auto;
+.wrapper {
+  width: 1200px;
+  height: auto;
+  margin: 0 auto;
 }
 </style>
---
+++ src/components/FooterBlock.vue
@@ -0,0 +1,30 @@
+<template>
+  <footer>
+    <div class="wrapper">
+      <div class="header_link_block">
+        <a href="mainpage.html" class="header_link">Жанры</a>
+        <a href="mainpage.html" class="header_link">Подборки</a>
+        <a href="mainpage.html" class="header_link">Новинки</a>
+        <a href="mainpage.html" class="header_link">Топовые обзоры</a>
+        <a href="mainpage.html" class="header_link">Войти</a>
+      </div>
+    </div>
+  </footer>
+</template>
+<style scoped>
+a {
+  text-decoration: none;
+  color: white;
+  font-size: 12px;
+}
+footer {
+  background: linear-gradient(90deg, #110818 0%, #13101d 100%);
+  height: 100px;
+}
+.header_link_block {
+  padding-top: 10px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+</style>
